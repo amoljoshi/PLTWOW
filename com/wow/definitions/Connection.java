275ea578e8ed802ea0ff5e1f3fe198091ae1da72
Yash Parikh <parikh.yash21@gmail.com>
2014-04-16 00:10:32 -0400

    Added cycle detection algo. Minor changes to Connection.java as well.

--- com/wow/definitions/Connection.java
+++ com/wow/definitions/Connection.java
@@ -2,21 +2,24 @@ package com.wow.definitions;
 import java.util.*;
 
 public class Connection{
+	static int id = 1;
+	int connectionId;
 	ArrayList<ArrayList<String>> connection;
 	public Connection(){
+		connectionId = id++;
 		connection = new ArrayList<ArrayList<String>> ();
 	}
-	public void addConnection(String from, String to){
+	public void addConnection(String originNode, String destinationNode){
 		ArrayList<String> newConnection = new ArrayList<String>();
-		newConnection.add(from);
-		newConnection.add(to);
+		newConnection.add(originNode);
+		newConnection.add(destinationNode);
 		connection.add(newConnection);
 	}
 	public String toString(){
 		String s = new String();
-		s += "\nConnections defined as follow : \n";
+		s += "\nConnections defined as follows:\n";
 		for(ArrayList<String> each : connection){
-			s += "\tConnection from " + each.get(0) + " to " + each.get(1);
+			s += "\t"+connectionId+". Connection from " + each.get(0) + " to " + each.get(1);
 		}
 		return s;
 	}
---
+++ com/wow/definitions/CycleDetection.java
@@ -0,0 +1,87 @@
+package com.wow.definitions;
+import java.util.*;
+
+class CycleDetection
+{
+	//ceck if the workflow entered by the user contains any cycles
+	static boolean detectCycles(Connection connections)
+	{
+		//initialization
+		ArrayList<ArrayList<String>> connectionsList = new ArrayList<ArrayList<String>>(connections.connection);
+
+		String startNode = connections.connection.get(0).get(0);
+
+		//use the code below to deep copy in case the constructor doesnt work
+		/*connectionsList = new ArrayList<ArrayList<String>>(connections);
+		for(ArrayList<String> connection: connections.connection)
+		{
+			ArrayList<String> singleConnection = new ArrayList<String>();	
+			for(Sting node: connection)
+			{
+				singleConnection.add(node);
+			}
+			connectionsList.add(singleConnection);
+		}*/
+
+		//initialize and create the adjacency list
+		Map<String, <ArrayList<String>>> adjacencyList = new HashMap<String, <ArrayList<String>>>();
+		// Set<String> visitedSet = new HashSet<String>();
+		// List<String> nodeList = new LinkedList<String>();
+		//formation of the adjacency list and formation of node list
+		for(ArrayList<String> connection: connectionsList)
+		{
+			String source = connection.get(0);
+			String destination = connection.get(1);
+
+			/*if(!visitedSet.contains(source))
+			{
+				visitedSet.add(source);
+				nodeList.add(source);
+			}
+
+			if(!visitedSet.contains(destination))
+			{
+				visitedSet.add(destination);
+				nodeList.add(destination);
+			}*/
+
+			if(adjacencyList.keyExists(source))
+			{
+				adjacencyList.get(source).add(destination);
+			}
+			else
+			{
+				ArrayList<String> valueList = new ArrayList<String>();
+				valueList.add(destination);
+				adjacencyList.put(source, valueList);
+			}
+		}
+
+		Set<String> visitedSet = new HashSet<String>();
+		LinkedList<String> frontierList = new LinkedList<String>();
+
+		//add the start node of the workflow and begin BFS
+		frontierList.add(startNode);
+
+		//continue till all nodes have been visited
+		while(!frontierList.isEmpty())
+		{
+			String n = frontierList.remove();
+			visitedSet.add(n);
+
+			//for each connected node of the current node, check if it is there in the visited set
+			for(String node: adjacencyList.get(n))
+			{
+				//if it is already contained in the visited set, then the workflow has a cycle
+				if(visitedSet.contains(node))
+				{
+					return false;
+				}
+				//if it is not visited yet, add it to the frontier list
+				frontierList.add(node);
+			}
+		}
+		//successful exit from the loop indicates no cycle
+		return true;
+	}
+}
