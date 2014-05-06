82455fa8c749ccf8794107eefbdfd2c78743b4ed
Yash Parikh <parikh.yash21@gmail.com>
2014-05-06 17:18:19 -0400

    ExpressionNode updated in the AST

--- com/wow/ast/ExpressionNode.java
+++ com/wow/ast/ExpressionNode.java
@@ -37,9 +37,69 @@ public class ExpressionNode extends ASTNode {
 		type = n.type;
 	}
 
-	public ExpressionNode (ExpressionNode n1, String operator, ExpressionNode n3) {
+	public ExpressionNode (ExpressionNode node1, String operator, ExpressionNode n3) {
 		//implementation remaining
+		children.add(node1);
+		op = operator;
+		children.add(n3);
+		if (!node1.type.equals(n3.type) && (node1.type.equals("int") && n3.type.equals("double") || node1.type.equals("double") && n3.type.equals("int")
+									  || node1.type.equals("string") && n3.type.equals("int") || node1.type.equals("int") && n3.type.equals("string")
+									  || node1.type.equals("string") && n3.type.equals("double") || node1.type.equals("double") && n3.type.equals("string")))
+			System.err.println("Error: Cannot perform the operation " + node1.type + " " + op + " " + n3.type);
+		else {
+			if (op.equals("+")) {
+				if (node1.type.equals("int") && n3.type.equals("int"))
+					type = "int";
+				else if((node1.type.equals("double") && n3.type.equals("int")))
+					type = "double";
+				else if((node1.type.equals("int") && n3.type.equals("double")))
+					type = "double";
+				else if ((node1.type.equals("double")||node1.type.equals("int"))&&n3.type.equals("string"))
+					type = "string";
+				else if ((n3.type.equals("double")||n3.type.equals("int"))&&node1.type.equals("string"))
+					type = "string";
+				else
+					System.err.println("Error: Cannot perform the operation "+node1.type+" "+op+" "+n3.type);
+			}
 
+			if (op.equals("-")||op.equals("*")||op.equals("/")||op.equals("^")||op.equals("%")) {
+				if (node1.type.equals("int") && n3.type.equals("int"))
+					type = "int";
+				else if((node1.type.equals("double") && n3.type.equals("int")))
+					type = "double";
+				else if((node1.type.equals("int") && n3.type.equals("double")))
+					type = "double";
+				else if((node1.type.equals("double") && n3.type.equals("double"))) type = "double";
+				else
+					System.err.println("Error: Cannot perform the operation "+node1.type+" "+op+" "+n3.type);
+			}
+
+			if (op.equals("<")||op.equals(">")||op.equals("<=")||op.equals(">=")) {
+				if (node1.type.equals("boolean")||node1.type.equals("string")||n3.type.equals("boolean")||n3.type.equals("string")) {
+					System.out.println("Error: Cannot perform the operation "+node1.type+" "+op+" "+n3.type);
+				}
+				else
+					type = "boolean";
+			}
+
+			if (op.equals("!=")||op.equals("==")) {
+				if (node1.type.equals("boolean")&& n3.type.equals("boolean"))
+					type = "boolean";
+				else if ((node1.type.equals("double")||node1.type.equals("int"))&&(n3.type.equals("double")||n3.type.equals("int")))
+					type = "boolean";
+				else
+					System.err.println("Error: Cannot perform the operation "+node1.type+" "+op+" "+n3.type);
+
+			}
+
+			if (op.equals ("and") || op.equals("or")) {
+				if (node1.type.equals("boolean") && n3.type.equals("boolean"))
+					type = "boolean";
+			else 
+				System.err.println("Error: Cannot perform the operation "+node1.type+" "+op+" "+n3.type);
+
+			}
+		}
 	}
 
 
