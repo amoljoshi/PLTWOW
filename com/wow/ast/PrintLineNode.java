61b02b77239f008de1bf7d17e3c09cffb69bc8ee
amoljoshi <aj2568@columbia.edu>
2014-05-07 17:36:27 -0400

    PrintLineNode changes, Second.WoW has endblock now

---
+++ com/wow/ast/PrintLineNode.java
@@ -0,0 +1,16 @@
+package com.wow.ast;
+public class PrintLineNode extends ASTNode{
+	public PrintLineNode(ExpressionNode expr){
+		// if(!expr.type.startsWith("LibraryFunctions"))
+			children.add(expr);
+		// else{
+			// System.err.println("Error: Can't print " + expr.type);
+		// }
+	}
+	public String toString(){
+		if(children.get(0) != null){
+			return "System.out.println(" + children.get(0) + ")";
+		}
+		return "System.out.println()";
+	}
+}
